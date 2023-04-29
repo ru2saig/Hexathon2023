f4c0eebff61b6e05839abca00085d1eb7df2c196
rishi <rishi88221@gmail.com>
2023-04-30 02:18:16 +0530

    Finding the path

--- tbd-ideav2/src/pages/api/get-data.js
+++ tbd-ideav2/src/pages/api/get-data.js
@@ -6,22 +6,54 @@ const client = new Client()
 const account = new Databases(client);
         
 export default function handler(req, res) {
+     
     if (req.method === 'GET') {
         const databases = new Databases(client);
-
+        const docid = ID.unique();
+        
         const promise = databases.createDocument(
             '644d06dd4576ea7fbaad',
             '644d0abfd58662934852',
-            ID.unique(),
+            `${docid}`,
             {
-                'dustbin-lat': 17.496380,
-                'dusbin-lng': 78.403267,
+                'dustbin-lat': 17.497930,
+                'dusbin-lng': 78.403917,
                 'area': 'bhagyanagar kphb',
-                'last-collected': '2023-04-07T00:00Z',
+                'last-collected': '2023-04-27T12:00Z',
                 'dustbin-fullness': 46,
             }
             
         );
+        let vsar = databases.getDocument(
+        '644d06dd4576ea7fbaad',
+        '644d0abfd58662934852',
+        `${docid}`);
+        console.log(vsar)
+        // .then((response) => {
+        //     let lat = response.response['dustbin-lat']
+        //     let lng = response.response['dusbin-lng'];
+        //     console.log(lat,lng)
+        //     databases.listDocuments('644d06dd4576ea7fbaad',
+        //         '644d0abfd58662934852').then((responqse) => {
+        //             let a = [];
+        //             let c = [];
+        //             for(let i = 0; i< responqse.documents.length; i++){
+        //                 let d,b = fetchosrmres(responqse.documents[i],lat,lng)
+        //                 a.push(d);
+        //                 c.push(b);
+        //             }
+        //             var min = 100000000
+        //             for(var p = 0;p< c.length();c++){
+        //                 min = c[p] < min ? c[p]:min;
+        //             }
+        //             let add = c.findIndex(min)
+        //             console.log(add)
+        //         })
+            
+            
+        // })
+        
+        
 
         promise.then(function (response) {
             console.log(response);
