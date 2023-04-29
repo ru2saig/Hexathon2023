bd669724e2916c4d942fe325d2ac43d88d7c8c6e
Nidhish (ru2saig) <42857541+ru2saig@users.noreply.github.com>
2023-04-30 03:57:16 +0530

    Resolved pending merge conflicts

--- tbd-ideav2/next.config.js
+++ tbd-ideav2/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-  reactStrictMode: true,
-=======
   reactStrictMode: false,
->>>>>>> api
 }
 
 module.exports = nextConfig
