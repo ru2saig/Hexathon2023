36f80cbd6aedee0de8133c7081c71d165497b65b
Anirban Sikdar <anirban9293@gmail.com>
2023-04-29 14:03:28 +0530

    Update

--- tbd-idea/src/app/api/hello/route.js
+++ tbd-idea/src/app/api/hello/route.js
@@ -1,3 +1,13 @@
-export async function GET(request) {
-  return new Response('Hello, Next.js!')
+export default function handler(req, res) {
+  const requestMethod = req.method;
+  const body = JSON.parse(req.body);
+  switch (requestMethod) {
+    case 'POST':
+      res.status(200).json({ message: `You submitted the following data: ${body}` })
+      
+    // handle other HTTP methods
+    default:
+      res.status(200).json({ message: 'Welcome to API Routes!'})
+  }
 }
+
