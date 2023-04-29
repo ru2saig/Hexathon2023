292aa3b54bfd32852306d4b6f1a6f99b9ff1b0a1
Anirban Sikdar <anirban9293@gmail.com>
2023-04-29 20:34:13 +0530

    Response added

--- tbd-ideav2/src/pages/api/get-bin.js
+++ tbd-ideav2/src/pages/api/get-bin.js
@@ -1,11 +1,11 @@
 import { Client, Account, ID } from "appwrite";
 
 const client = new Client()
-    .setEndpoint('https://http://100.104.213.57:4172/v1') // Your API Endpoint
+    .setEndpoint('https://100.104.213.57:4172/v1') // Your API Endpoint
     .setProject('644cfafd2592bab3681f');               // Your project ID
 
 const account = new Account(client);
-        
+
 export default function handler(req, res) {
     if (req.method === 'GET') {
         account.create(
@@ -15,8 +15,10 @@ export default function handler(req, res) {
             'Jane Doe'
         ).then(response => {
             console.log(response);
+            res.status(200).json({ message: 'Account created successfully!' });
         }, error => {
             console.log(error);
+            res.status(400).json({message: 'Account not created!!'})
         });
     }
 }
