0d0f5e71239e45d5d94f4ce3ef542d0aaef5dfe2
Anirban Sikdar <anirban9293@gmail.com>
2023-04-29 19:54:06 +0530

    Appwrite test code

--- tbd-ideav2/src/pages/api/get-bin.js
+++ tbd-ideav2/src/pages/api/get-bin.js
@@ -1,5 +1,22 @@
 import { Client, Account, ID } from "appwrite";
 
 const client = new Client()
-    .setEndpoint('https://[HOSTNAME_OR_IP]/v1') // Your API Endpoint
-    .setProject('5df5acd0d48c2');               // Your project ID
+    .setEndpoint('https://http://100.104.213.57:4172/v1') // Your API Endpoint
+    .setProject('644cfafd2592bab3681f');               // Your project ID
+
+const account = new Account(client);
+        
+export default function handler(req, res) {
+    if (req.method === 'GET') {
+        account.create(
+            ID.unique(),
+            'me@example.com',
+            'password',
+            'Jane Doe'
+        ).then(response => {
+            console.log(response);
+        }, error => {
+            console.log(error);
+        });
+    }
+}
