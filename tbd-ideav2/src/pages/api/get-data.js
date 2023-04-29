dae9c6f84d69ed1b07c67b3fadb6119f5507e4f5
Nidhish (ru2saig) <42857541+ru2saig@users.noreply.github.com>
2023-04-29 22:45:18 +0530

    Post request donegit add .

--- tbd-ideav2/src/pages/api/get-data.js
+++ tbd-ideav2/src/pages/api/get-data.js
@@ -4,6 +4,28 @@ const client = new Client()
     .setEndpoint('http://100.104.213.57:4172/v1') // Your API Endpoint
     .setProject('644cfafd2592bab3681f');
 const account = new Databases(client);
+
+function createData( dustbin_lat, dusbin_lng ,area ,last_collected,dustbin_fullness ){
+    const promise = databases.createDocument(
+        '644d06dd4576ea7fbaad',
+        '644d0abfd58662934852',
+        ID.unique(),
+        {
+            'dustbin-lat': dustbin_lat,
+            'dusbin-lng': dusbin_lng,
+            'area': area,
+            'last-collected': last_collected,
+            'dustbin-fullness': dustbin_fullness,
+        }
+    );
+        promise.then(function (response) {
+            console.log(response);
+            res.status(200).json({ message: 'Database Created!', response: response });
+        }, function (error) {
+            console.log(error);
+            res.status(200).json({ message: 'Error!', response:error });
+        });
+};
         
 export default function handler(req, res) {
     if (req.method === 'GET') {
@@ -30,5 +52,8 @@ export default function handler(req, res) {
             console.log(error);
             res.status(200).json({ message: 'Error!', response:error });
         });
+    }if (req.method === 'GET') {
+        const { dustbin_lat, dusbin_lng ,area ,last_collected,dustbin_fullness } = req.body;
+        createData(dustbin_lat, dusbin_lng, area, last_collected, dustbin_fullness );
     }
 }
