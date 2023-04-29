b72dc1cdb0b1dd0eda1a1cfca0ab3742fe8341f6
tanmaypradhan4112 <tanmaypradhan4112@gmail.com>
2023-04-29 12:46:37 +0530

    added tbd-idea

---
+++ tbd-idea/src/app/page.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='w-screen h-full'>
+      {/* Header function */}
+      <div className='header text-center text-2xl font-bold'>
+        <h1>Trash Track</h1>
+        <p className='text-xl font-light'>Navigate the city with Optimal Dustbin Collection</p>
+      </div>
+
+      {/* Map container */}
+      <div className='w-full h-[544px] bg-slate-700'>
+          <div>
+
+          </div>
+      </div>
+
+      {/* driver details */}
+      <div className=''>
+
+      </div>
+    </div>
+  )
+}
